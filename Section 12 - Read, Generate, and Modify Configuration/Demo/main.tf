86a47dbeef51071c9143f1f943b3cb9724b8ae09
9QIX <kirkhendrixbulaong@outlook.com>
2026-06-30 20:08:06 +0800

    Add input variables, outputs, and resource configurations for AWS infrastructure setup

--- Section 12 - Read, Generate, and Modify Configuration/Demo/main.tf
+++ Section 12 - Read, Generate, and Modify Configuration/Demo/main.tf
@@ -29,7 +29,7 @@ locals {
     Service   = local.service_name
     AppTeam   = local.app_team
     CreatedBy = local.createdby
- } 
+  }
 }
 #Define the VPC
 resource "aws_vpc" "vpc" {
@@ -42,12 +42,43 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+#Random ID for unique resource naming
+resource "random_id" "randomness" {
+  byte_length = 16
+}
+
+#Deploy the private subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value % length(data.aws_availability_zones.available.names)]
+
+  tags = {
+    Name      = each.key
+    Terraform = "true"
+  }
+}
+
+#Deploy the variables subnet
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
+}
+
 #Deploy the public subnets
 resource "aws_subnet" "public_subnets" {
   for_each                = var.public_subnets
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value % length(data.aws_availability_zones.available.names)]
   map_public_ip_on_launch = true
 
   tags = {
@@ -78,12 +109,70 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+#Create Elastic IP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    Name = "demo_igw_eip"
+  }
+}
+
+#Create NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "demo_nat_gateway"
+  }
+}
+
+#Create route table for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = {
+    Name      = "demo_private_rtb"
+    Terraform = "true"
+  }
+}
+
+#Create route table associations for private subnets
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
 #Create route table associations
 resource "aws_route_table_association" "public" {
   depends_on     = [aws_subnet.public_subnets]
   route_table_id = aws_route_table.public_route_table.id
   for_each       = aws_subnet.public_subnets
   subnet_id      = each.value.id
+}
+
+# Terraform Data Block - To Lookup Latest Ubuntu 16.04 AMI Image
+data "aws_ami" "ubuntu_16_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
 }
 
 # Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
@@ -101,6 +190,36 @@ data "aws_ami" "ubuntu" {
   }
 
   owners = ["099720109477"]
+}
+
+# Terraform Resource Block - S3 Bucket
+resource "aws_s3_bucket" "my-new-S3-bucket" {
+  bucket = "my-new-tf-test-bucket-${random_id.randomness.hex}"
+
+  tags = {
+    Name    = "my-new-S3-bucket"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
+}
+
+# Terraform Resource Block - New Security Group
+resource "aws_security_group" "my-new-security-group" {
+  name        = "web_server_inbound"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Allow inbound traffic on tcp/443"
+
+  ingress {
+    description = "Allow 443 from the internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "Intro to Resource Blocks Lab"
+  }
 }
 
 # Terraform Resource Block - Security Group to Allow Web Traffic
--- Section 12 - Read, Generate, and Modify Configuration/Demo/variables.tf
+++ Section 12 - Read, Generate, and Modify Configuration/Demo/variables.tf
@@ -12,7 +12,26 @@ variable "public_subnets" {
   default = {
     "public_subnet_1" = 1
     "public_subnet_2" = 2
+    "public_subnet_3" = 3
   }
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "variables_sub_cidr" {
+  type    = string
+  default = "10.0.200.0/24"
+}
+
+variable "variables_sub_auto_ip" {
+  type    = bool
+  default = false
 }
 
 variable "environment" {
@@ -29,3 +48,5 @@ variable "server_name" {
   type    = string
   default = "demo-server"
 }
+
+
